757fbfbddc65da2d7b76fe47508c51d05e5d3074
Devoxin <lukel9914@gmail.com>
2020-04-26 00:28:30 +0100

    mm

--- src/main/kotlin/me/stylite/predator/cogs/Apex.kt
+++ src/main/kotlin/me/stylite/predator/cogs/Apex.kt
@@ -12,13 +12,25 @@ import me.stylite.predator.utils.Http
 import me.stylite.predator.utils.RandomItems
 import net.dv8tion.jda.api.EmbedBuilder
 
-val colors = mapOf("Bangalore" to 0x7c635f, "Bloodhound" to 0xc14340, "Caustic" to 0xcaa757, "Crypto" to 0xbbd266, "Gibraltar" to 0xe5e0da, "Lifeline" to 0x8eb5e0, "Mirage" to 0xe49419, "Octane" to 0x999a54, "Pathfinder" to 0xfafd69, "Revenant" to 0x9c5052, "Wattson" to 0xe2914f, "Wraith" to 0x545ca2)
-
 class Apex : Cog {
     private val http = Http()
     private val gson = Gson()
 
     private val validPlatforms = setOf("X1", "PS4", "PC")
+    private val colors = mapOf(
+        "Bangalore" to 0x7c635f,
+        "Bloodhound" to 0xc14340,
+        "Caustic" to 0xcaa757,
+        "Crypto" to 0xbbd266,
+        "Gibraltar" to 0xe5e0da,
+        "Lifeline" to 0x8eb5e0,
+        "Mirage" to 0xe49419,
+        "Octane" to 0x999a54,
+        "Pathfinder" to 0xfafd69,
+        "Revenant" to 0x9c5052,
+        "Wattson" to 0xe2914f,
+        "Wraith" to 0x545ca2
+    )
 
     @Command(description = "Get stats of a player")
     suspend fun stats(ctx: Context, platform: String, username: String) {
@@ -26,6 +38,7 @@ class Apex : Cog {
 
         if (platformUpper !in validPlatforms) {
             return ctx.send("Invalid platform. ${validPlatforms.joinToString("`, `", prefix = "`", postfix = "`")}")
+        }
 
         val stats = http.fetchStats(platformUpper, username)
 
@@ -41,15 +54,15 @@ class Apex : Cog {
         }
 
         val user = gson.fromJson(response, ApexProfile::class.java)
-        val stat = StringBuilder("Current legend: ${user.legends.selected.LegendName}\n")
+        val stat = StringBuilder("**Current legend**: ${user.legends.selected.LegendName}\n")
 
         for (legend in user.legends.selected.data) {
-            stat.append(legend.name).append(": ").append(legend.value).append("\n")
+            stat.append("**").append(legend.name).append("**: ").append(legend.value).append("\n")
         }
 
         ctx.send {
-            setTitle("Stats for ${user.global.name} [$platform]")
             setColor(colors[user.legends.selected.LegendName] ?: 0xffffff)
+            setTitle("Stats for ${user.global.name} [$platformUpper]")
             setDescription(stat.toString())
             setThumbnail(user.legends.selected.ImgAssets.icon)
             setFooter("Info provided https://mozambiquehe.re/")
@@ -70,12 +83,12 @@ class Apex : Cog {
     suspend fun ranked(ctx: Context, platform: String, username: String) {
         val stats = http.fetchStats(platform, username)
         val user = gson.fromJson(stats.body(), ApexProfile::class.java)
-        val stat = StringBuilder("Current rank: ${user.global.rank.rankName}\n")
-            .append("Ranked score: ${user.global.rank.rankScore}\n")
-            .append("Ranked division: ${user.global.rank.rankDiv}")
+        val stat = StringBuilder("**Current rank**: ${user.global.rank.rankName}\n")
+            .append("**Ranked score**: ${user.global.rank.rankScore}\n")
+            .append("**Ranked division**: ${user.global.rank.rankDiv}")
 
         ctx.send {
-            setTitle("Stats for ${user.global.name} [$platform]")
+            setTitle("Stats for ${user.global.name} [${platform.toUpperCase()}]")
             setDescription(stat.toString())
             setThumbnail(user.global.rank.rankImg)
             setFooter("Info provided https://mozambiquehe.re/")
@@ -86,13 +99,13 @@ class Apex : Cog {
     suspend fun global(ctx: Context, platform: String, username: String) {
         val stats = http.fetchStats(platform, username)
         val user = gson.fromJson(stats.body(), ApexProfile::class.java)
-        val stat = StringBuilder()
-        stat.append("**Level**: ${user.global.level}\n")
+        val stat = StringBuilder("**Level**: ${user.global.level}\n")
             .append("**Rank**: ${user.global.rank.rankName}\n")
             .append("**Percent to next level**: ${user.global.toNextLevelPercent}\n")
             .append("**Battlepass level**: ${user.global.battlepass.level}\n")
+
         ctx.send {
-            setTitle("Stats for ${user.global.name} [$platform]")
+            setTitle("Stats for ${user.global.name} [${platform.toUpperCase()}]")
             setThumbnail(user.legends.selected.ImgAssets.icon)
             setDescription(stat.toString())
             setFooter("Info provided https://mozambiquehe.re/")
