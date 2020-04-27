d8ee4e712f524a237ba7c3fe9d9e20fe3d64c559
Devoxin <lukel9914@gmail.com>
2020-04-27 18:22:59 +0100

    Offer way to specify font path

--- src/main/kotlin/me/stylite/predator/utils/Resources.kt
+++ src/main/kotlin/me/stylite/predator/utils/Resources.kt
@@ -1,6 +1,8 @@
 package me.stylite.predator.utils
 
 import java.awt.Font
+import java.io.File
+import java.io.IOException
 import java.io.InputStream
 
 object Resources {
@@ -12,7 +14,11 @@ object Resources {
     val battlepass: InputStream
         get() = this::class.java.classLoader.getResourceAsStream("battlepass.png")!!
 
-    val font = Font.createFont(Font.TRUETYPE_FONT, defaultFont)
+    val font = try {
+        Font.createFont(Font.TRUETYPE_FONT, defaultFont)
+    } catch (e: IOException) {
+        Font.createFont(Font.TRUETYPE_FONT, File(System.getenv("FONT_PATH")))
+    }
 
     fun legend(name: String) = this::class.java.classLoader.getResourceAsStream("legends/$name.png")!!
     fun rank(name: String) = this::class.java.classLoader.getResourceAsStream("ranks/$name.png")!!
